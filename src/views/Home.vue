5cffd5ae8e3511c6eadbc6365973f896e85f824f
katerina-shche <asti.katerina@gmail.com>
2023-11-24 13:11:36 +0100

    composables: getPost() and getPosts()

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,27 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>Search term - {{ search }}</p>
-    <div v-for='name in matchingNames' :key='name'>{{ name }}</div>
-    <button @click='handleClick'>Stop Watching</button>
+    <div v-if='error'>{{error}}</div>
+    <PostList v-if='posts.length' :posts='posts'/>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-import { ref, computed, watch, watchEffect } from 'vue'
+import getPosts from '../composables/getPosts'
+import PostList from '../components/PostList.vue'
+
 
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['kate', 'igor', 'bella'])
+    const {posts, error, load } = getPosts()
+  
+    load()
 
-    const stopWatching = watch(search, () => {
-      console.log('I watch the search')
-    })
-    const stopWatchingEffect = watchEffect(() => {
-      console.log('watchEffect function runs', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter(name => name.includes(search.value))
-  })
-    //returning watch and watchEffect functions to stop them working
-    const handleClick = () => {
-      stopWatching()
-      stopWatchingEffect()
-    }
-    return {names, search, matchingNames, handleClick}
+    return { posts, error }
   }
 }
 </script>
